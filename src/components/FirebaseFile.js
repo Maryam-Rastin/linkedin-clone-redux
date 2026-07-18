4e7fe500cd2c04c2daed7c294b4121e0fe31ed68
Maryam-Rastin <248294243+Maryam-Rastin@users.noreply.github.com>
2026-07-18 16:26:44 +0330

    handling security risks

--- src/components/FirebaseFile.js
+++ src/components/FirebaseFile.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArDyyXebXO4S-JhPx9Ht1LaSDVtD9YP1U",
-  authDomain: "linkedin-clone-834cc.firebaseapp.com",
-  projectId: "linkedin-clone-834cc",
-  storageBucket: "linkedin-clone-834cc.firebasestorage.app",
-  messagingSenderId: "12228059124",
-  appId: "1:12228059124:web:dec3a31cbb49d6ac58a3a1",
-};
+     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
